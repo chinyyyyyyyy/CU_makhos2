9e0f18d723e40f9853fd1905ddcf39b48ac3f712
chinyyyyyyyy <chinopsw2@gmail.com>
2020-03-31 20:48:29 +0700

    add part to colab

---
+++ main_colab.py
@@ -0,0 +1,40 @@
+from Coach_th_checkers import Coach
+from ThaiCheckers.ThaiCheckersGame import ThaiCheckersGame as Game
+from utils import dotdict
+import os
+
+"""
+Before using multiprocessing, please check 2 things before use this script.
+1. The number of PlayPool should not over your CPU's core number.
+2. Make sure all Neural Network which each process created can store in VRAM at same time. Check your NN size before use this.
+"""
+
+args = dotdict({
+    'numIters': 500,
+    'numEps': 50,  # 25000
+    'tempThreshold': 15,  # not used
+    'updateThreshold': 0.55,  # not used
+    'maxlenOfQueue': 200000,
+    'numMCTSSims': 100,  # 1600 , 800
+    'arenaCompare': 100,  # 400, 0
+    'cpuct': 2,
+
+    'multiGPU': False,
+    'setGPU': '0',
+    'numSelfPlayPool': 2,
+    'numTestPlayPool': 2,
+
+    'checkpoint': '/root/test/CU_Makhos/models_colab/',
+    'load_model': True,
+    'load_iter': 274,
+    'load_folder_file': '/root/test/CU_Makhos/models_colab/',
+    'numItersForTrainExamplesHistory': 4 , # 4
+})
+
+if __name__ == "__main__":
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+    g = Game()
+    c = Coach(g, args)
+    c.learn()
+
